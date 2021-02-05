37d588018e811d34ff72c3ae8e27281b6722b177
sunriseonzebes <76784408+sunriseonzebes@users.noreply.github.com>
2021-02-05 16:35:14 -0500

    Add TRConfig.original_rom_path, original_rom properties

--- tests/testing_common.py
+++ tests/testing_common.py
@@ -3,3 +3,4 @@ import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "", "..")))
 
 import tile_rando
+original_rom_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "fixtures", "original_rom.sfc")
--- tile_rando/tr_config.py
+++ tile_rando/tr_config.py
@@ -1,6 +1,28 @@
+import os
+
 class TRConfig:
     def __init__(self):
         self._original_rom_path = ""
         self._original_rom = b''
         self._modified_rom_path = ""
         self._seed = None
+
+    @property
+    def original_rom_path(self):
+        """str: The path to the original rom file."""
+        return self._original_rom_path
+
+    @original_rom_path.setter
+    def original_rom_path(self, new_rom_path):
+        if not os.path.exists(new_rom_path):
+            raise OSError("{} does not exist!".format(new_rom_path))
+        self._original_rom_path = new_rom_path
+        self._load_original_rom()
+
+    def _load_original_rom(self):
+        with open(self._original_rom_path, 'rb') as f:
+            self._original_rom = f.read()
+
+    @property
+    def original_rom(self):
+        return self._original_rom
