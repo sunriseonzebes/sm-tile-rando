a4e7a2e86b03cc09361400e0097cd5553b8c30e0
sunriseonzebes <76784408+sunriseonzebes@users.noreply.github.com>
2021-02-06 10:45:44 -0500

    Add parse_args function to TRProject

--- tests/test_tr_project.py
+++ tests/test_tr_project.py
@@ -1,5 +1,6 @@
+import os
 import unittest
-from testing_common import tile_rando, original_rom_path
+from testing_common import tile_rando, original_rom_path, load_test_data_dir
 
 from tile_rando import tr_project
 
@@ -10,3 +11,21 @@ class TestTRProject(unittest.TestCase):
         self.assertTrue(isinstance(test_project, tr_project.TRProject))
         self.assertEqual("", test_project.original_rom_path, "TRProject _original_rom_path did not initialize properly!")
         self.assertEqual("", test_project.modified_rom_path, "TRProject _modified_rom_path did not initialize properly!")
+
+    def test_parse_args(self):
+        test_project = tr_project.TRProject()
+        test_project.parse_args(['-i', 'original_rom.sfc', '-o', 'modified_rom.sfc'])
+        self.assertEqual('original_rom.sfc',
+                         test_project.original_rom_path,
+                         "parse_args did not load original_rom_path correctly!")
+        self.assertEqual('modified_rom.sfc',
+                         test_project.modified_rom_path,
+                         "parse_args did not load modified_rom_path correctly!")
+        with self.assertRaises(SystemExit):
+            test_project.parse_args([])
+        with self.assertRaises(SystemExit):
+            test_project.parse_args(['-t', 'nonsense_arg'])
+        with self.assertRaises(SystemExit):
+            test_project.parse_args(['-i', 'original_rom.sfc'])
+        with self.assertRaises(SystemExit):
+            test_project.parse_args(['-o', 'modified_rom.sfc'])
--- tile_rando/tr_project.py
+++ tile_rando/tr_project.py
@@ -1,4 +1,29 @@
+import argparse
+import sys
+
+
 class TRProject:
     def __init__(self):
         self.original_rom_path = ""
         self.modified_rom_path = ""
+
+    def parse_args(self, new_args=None):
+        if new_args is None:
+            new_args = sys.argv[1:]
+
+        parser = argparse.ArgumentParser(
+            description='Randomize tiles, rooms, map structure and powerups of a Super Metroid ROM.'
+        )
+        parser.add_argument('-i',
+                            metavar="<input_rom_file>",
+                            type=str,
+                            required=True,
+                            help="Path to unmodified ROM to use as a source file.")
+        parser.add_argument('-o', metavar="<output_rom_file>",
+                            type=str,
+                            required=True,
+                            help="Path to write modified ROM to.")
+        args = parser.parse_args(new_args)
+
+        self.original_rom_path = args.i
+        self.modified_rom_path = args.o
