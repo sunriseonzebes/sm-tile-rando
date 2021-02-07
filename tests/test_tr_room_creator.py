8bfc9bd68e1edc0395aab2f182bd7f348d31fb1a
sunriseonzebes <76784408+sunriseonzebes@users.noreply.github.com>
2021-02-07 14:47:24 -0500

    Stub out TRRoomCreator.generate_map_grid

--- tests/test_tr_room_creator.py
+++ tests/test_tr_room_creator.py
@@ -2,7 +2,8 @@ import os
 import unittest
 from testing_common import tile_rando, original_rom_path, load_test_data_dir
 
-from tile_rando import tr_room_creator
+from tile_rando import tr_room_creator, tr_map_grid
+from tekton import tekton_room_dict
 
 
 class TestTRRoomCreator(unittest.TestCase):
@@ -12,3 +13,14 @@ class TestTRRoomCreator(unittest.TestCase):
                         msg="TRRoomCreator did not initialize properly!")
         self.assertIsNone(test_creator.rooms,
                           msg="TRRoomCreator rooms did not initialize properly!")
+
+    def test_generate_map_grid(self):
+        test_dict = tekton_room_dict.TektonRoomDict()
+        test_creator = tr_room_creator.TRRoomCreator()
+        test_creator.rooms = test_dict
+        actual_result = test_creator.generate_map_grid()
+        self.assertTrue(isinstance(actual_result, tr_map_grid.TRMapGrid),
+                        msg="TRRoomCreator.generate_map_grid did not return a TRMapGrid object!")
+        for col in range(actual_result.width):
+            for row in range(actual_result.height):
+                self.assertIsNone(actual_result[col][row])
--- tile_rando/tr_room_creator.py
+++ tile_rando/tr_room_creator.py
@@ -1,3 +1,10 @@
+from .tr_map_grid import TRMapGrid
+
 class TRRoomCreator:
     def __init__(self):
         self.rooms = None
+
+    def generate_map_grid(self):
+        return_grid = TRMapGrid()
+
+        return return_grid
