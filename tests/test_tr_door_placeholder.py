55a0e6e5c065d71adf5c9501f4881be20e4d3f68
sunriseonzebes <76784408+sunriseonzebes@users.noreply.github.com>
2021-02-12 20:42:11 -0500

    Remove TRDoorAttachPoint

--- tests/test_tr_door_placeholder.py
+++ tests/test_tr_door_placeholder.py
@@ -14,15 +14,3 @@ class TestTRDoorPlaceholder(unittest.TestCase):
         self.assertEqual(0, test_ph.h_screen, "TRDoorPlaceholder h_screen did not initialize correctly!")
         self.assertEqual(0, test_ph.v_screen, "TRDoorPlaceholder v_screen did not initialize correctly!")
         self.assertIsNone(test_ph.exit_direction, "TRDoorPlaceholder exit_direction did not initialize correctly!")
-
-
-class TestTRDoorAttachPoint(unittest.TestCase):
-    def test_init(self):
-        test_attach = tr_door_placeholder.TRDoorAttachPoint(4, 2, [])
-        self.assertTrue(isinstance(test_attach, tr_door_placeholder.TRDoorAttachPoint),
-                        msg="TRDoorAttachPoint did not initialize correctly!")
-        self.assertEqual(4, test_attach.h_screen, "TRDoorAttachPoint h_screen did not initialize correctly!")
-        self.assertEqual(2, test_attach.v_screen, "TRDoorAttachPoint v_screen did not initialize correctly!")
-        self.assertEqual([],
-                         test_attach.allowed_door_exit_directions,
-                         "TRDoorAttachPoint allowed_door_exit_directions did not initialize correctly!")
--- tile_rando/tr_door_placeholder.py
+++ tile_rando/tr_door_placeholder.py
@@ -3,10 +3,3 @@ class TRDoorPlaceholder:
         self.h_screen = new_h_screen
         self.v_screen = new_h_screen
         self.exit_direction = new_exit_direction
-
-
-class TRDoorAttachPoint:
-    def __init__(self, new_h_screen, new_v_screen, new_allowed_directions):
-        self.h_screen = new_h_screen
-        self.v_screen = new_v_screen
-        self.allowed_door_exit_directions = new_allowed_directions
